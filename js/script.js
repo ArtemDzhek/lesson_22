651deedc370746629418093af47f6c3f00277dc3
ArtemDzhek <108084556+ArtemDzhek@users.noreply.github.com>
2022-12-20 21:32:12 +0100

    Add files via upload
    js

---
+++ js/script.js
@@ -0,0 +1,45 @@
+
+"use strict"
+
+
+let eyeColor;
+eyeColor = 'red';
+console.log(eyeColor);
+let user;
+
+let userName = 'Vasia';
+user = userName;
+console.log(user);
+
+
+let userAge = 36;
+let userInfo = `Фрілансер ${userAge}`;
+console.log(userInfo);
+
+let infoUser = {
+   name: "Vasia",
+   age: 33
+}
+console.log(infoUser);
+console.log(typeof infoUser);
+
+let userNumber = 33;
+console.log(userNumber);
+console.log(typeof userNumber);
+
+userNumber = String(userNumber);
+console.log(userNumber);
+console.log(typeof userNumber);
+
+let iStudy = 'Я вчу JS';
+console.log(iStudy);
+
+let oldUser;
+console.log(typeof oldUser);
+
+
+oldUser = 33;
+console.log(typeof oldUser);
+
+let userHeight = 145 / 0;
+console.log(userHeight);
